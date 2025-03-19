f71148e8dc74113fca074db46762214eee1071c7
luckys-lnz <lalucky22@gmail.com>
2025-03-19 15:35:29 +0000

    Add utility functions for Neovim configuration

---
+++ nvim/lua/utils/init.lua
@@ -0,0 +1,219 @@
+-- lua/utils/init.lua
+-- Utility functions
+
+local M = {}
+
+M.root_patterns = { ".git", "lua" }
+
+-- returns the root directory based on:
+-- * lsp workspace folders
+-- * lsp root_dir
+-- * root pattern of filename of the current buffer
+-- * root pattern of cwd
+---@return string
+function M.get_root()
+	---@type string?
+	local path = vim.api.nvim_buf_get_name(0)
+	path = path ~= "" and vim.loop.fs_realpath(path) or nil
+	---@type string[]
+	local roots = {}
+	if path then
+		for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+			local workspace = client.config.workspace_folders
+			local paths = workspace
+					and vim.tbl_map(function(ws)
+						return vim.uri_to_fname(ws.uri)
+					end, workspace)
+				or client.config.root_dir and { client.config.root_dir }
+				or {}
+			for _, p in ipairs(paths) do
+				local r = vim.loop.fs_realpath(p)
+				if path:find(r, 1, true) then
+					roots[#roots + 1] = r
+				end
+			end
+		end
+	end
+	table.sort(roots, function(a, b)
+		return #a > #b
+	end)
+	---@type string?
+	local root = roots[1]
+	if not root then
+		path = path and vim.fs.dirname(path) or vim.loop.cwd()
+		---@type string?
+		root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
+		root = root and vim.fs.dirname(root) or vim.loop.cwd()
+	end
+	---@cast root string
+	return root
+end
+
+-- this will return a function that calls telescope.
+-- cwd will default to lazyvim.util.get_root
+-- for `files`, git_files or find_files will be chosen depending on .git
+function M.telescope(builtin, opts)
+	local params = { builtin = builtin, opts = opts }
+	return function()
+		builtin = params.builtin
+		opts = params.opts
+		opts = vim.tbl_deep_extend("force", { cwd = M.get_root() }, opts or {})
+		if builtin == "files" then
+			if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
+				opts.show_untracked = true
+				builtin = "git_files"
+			else
+				builtin = "find_files"
+			end
+		end
+		if opts.cwd and opts.cwd ~= vim.loop.cwd() then
+			opts.attach_mappings = function(_, map)
+				map("i", "<a-c>", function()
+					local action_state = require("telescope.actions.state")
+					local line = action_state.get_current_line()
+					M.telescope(
+						params.builtin,
+						vim.tbl_deep_extend("force", {}, params.opts or {}, { cwd = false, default_text = line })
+					)()
+				end)
+				return true
+			end
+		end
+
+		require("telescope.builtin")[builtin](opts)
+	end
+end
+
+M.lsp = {}
+
+function M.lsp.on_attach(on_attach)
+	vim.api.nvim_create_autocmd("LspAttach", {
+		callback = function(args)
+			local buffer = args.buf
+			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			on_attach(client, buffer)
+		end,
+	})
+end
+
+function M.has(plugin)
+	return require("lazy.core.config").spec.plugins[plugin] ~= nil
+end
+
+function M.opts(name)
+	local plugin = require("lazy.core.config").plugins[name]
+	if not plugin then
+		return {}
+	end
+	local Plugin = require("lazy.core.plugin")
+	return Plugin.values(plugin, "opts", false)
+end
+
+function M.lsp.get_config(server)
+	local configs = require("lspconfig.configs")
+	return rawget(configs, server)
+end
+
+function M.lsp.disable(server, cond)
+	local util = require("lspconfig.util")
+	local def = M.lsp.get_config(server)
+	def.document_config.on_new_config = util.add_hook_before(
+		def.document_config.on_new_config,
+		function(config, root_dir)
+			if cond(root_dir, config) then
+				config.enabled = false
+			end
+		end
+	)
+end
+
+M.format = {}
+
+function M.format.formatter()
+	return {
+		name = "LSP",
+		primary = true,
+		priority = 1,
+		format = function(buf)
+			vim.lsp.buf.format({
+				bufnr = buf,
+				filter = function(client)
+					return client.name ~= "tsserver"
+				end,
+			})
+		end,
+		sources = function(buf)
+			local ret = {}
+			for _, client in ipairs(vim.lsp.get_active_clients({ bufnr = buf })) do
+				if client.supports_method("textDocument/formatting") then
+					ret[#ret + 1] = client.name
+				end
+			end
+			return ret
+		end,
+	}
+end
+
+function M.format.register(formatter)
+	local format = require("conform")
+	format.setup({
+		format_on_save = function(bufnr)
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			return { timeout_ms = 500, lsp_fallback = true }
+		end,
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			javascript = { { "prettierd", "prettier" } },
+		},
+	})
+
+	vim.api.nvim_create_user_command("FormatDisable", function(args)
+		if args.bang then
+			-- FormatDisable! will disable formatting just for this buffer
+			vim.b.disable_autoformat = true
+		else
+			vim.g.disable_autoformat = true
+		end
+	end, {
+		desc = "Disable autoformat-on-save",
+		bang = true,
+	})
+
+	vim.api.nvim_create_user_command("FormatEnable", function()
+		vim.b.disable_autoformat = false
+		vim.g.disable_autoformat = false
+	end, {
+		desc = "Re-enable autoformat-on-save",
+	})
+end
+
+function M.lsp.formatter()
+	return {
+		name = "LSP",
+		primary = true,
+		priority = 1,
+		format = function(buf)
+			vim.lsp.buf.format({
+				bufnr = buf,
+				filter = function(client)
+					return client.name ~= "tsserver"
+				end,
+			})
+		end,
+		sources = function(buf)
+			local ret = {}
+			for _, client in ipairs(vim.lsp.get_active_clients({ bufnr = buf })) do
+				if client.supports_method("textDocument/formatting") then
+					ret[#ret + 1] = client.name
+				end
+			end
+			return ret
+		end,
+	}
+end
+
+return M
