3a6f998f0899f11da6f9b12f11b4bac73f1b354c
luckys-lnz <lalucky22@gmail.com>
2025-04-01 15:09:14 +0000

    changed symbols and added more keybindings for last changed and more

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,25 +1,22 @@
+-- git settings
 return {
-	-- VS Code-like git integration
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
 		opts = {
 			signs = {
-				add = { text = "▎" },
-				change = { text = "▎" },
-				delete = { text = "契" },
-				topdelete = { text = "契" },
-				changedelete = { text = "▎" },
-				untracked = { text = "▎" },
+				add = { text = "" }, -- Plus sign for additions
+				change = { text = "" }, -- Pencil for changes
+				delete = { text = "󰍵" }, -- Trash bin for deletions
+				topdelete = { text = "󰍵" },
+				changedelete = { text = "󰏦" }, -- Modified delete icon
+				untracked = { text = "" }, -- Question mark for untracked files
 			},
 			signcolumn = true,
 			numhl = false,
 			linehl = false,
 			word_diff = false,
-			watch_gitdir = {
-				interval = 1000,
-				follow_files = true,
-			},
+			watch_gitdir = { interval = 1000, follow_files = true },
 			attach_to_untracked = true,
 			current_line_blame = false,
 			current_line_blame_opts = {
@@ -28,21 +25,19 @@ return {
 				delay = 1000,
 				ignore_whitespace = false,
 			},
-			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+			current_line_blame_formatter = " <author> - <author_time:%Y-%m-%d> | <summary>",
 			sign_priority = 6,
 			update_debounce = 100,
 			status_formatter = nil,
 			max_file_length = 40000,
 			preview_config = {
-				border = "single",
+				border = "rounded",
 				style = "minimal",
 				relative = "cursor",
 				row = 0,
 				col = 1,
 			},
-			yadm = {
-				enable = false,
-			},
+			yadm = { enable = false },
 			on_attach = function(bufnr)
 				local gs = package.loaded.gitsigns
 
@@ -57,47 +52,43 @@ return {
 					if vim.wo.diff then
 						return "]c"
 					end
-					vim.schedule(function()
-						gs.next_hunk()
-					end)
+					vim.schedule(gs.next_hunk)
 					return "<Ignore>"
-				end, { expr = true })
+				end, { expr = true, desc = "󰊢 Next Git Hunk" })
 
 				map("n", "[c", function()
 					if vim.wo.diff then
 						return "[c"
 					end
-					vim.schedule(function()
-						gs.prev_hunk()
-					end)
+					vim.schedule(gs.prev_hunk)
 					return "<Ignore>"
-				end, { expr = true })
+				end, { expr = true, desc = "󰊢 Previous Git Hunk" })
 
 				-- Actions
-				map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage Hunk" })
-				map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset Hunk" })
-				map("v", "<leader>hs", function()
+				map("n", "<leader>gs", gs.stage_hunk, { desc = " Stage Hunk" })
+				map("n", "<leader>gr", gs.reset_hunk, { desc = "󰅖 Reset Hunk" })
+				map("v", "<leader>gs", function()
 					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end, { desc = "Stage Hunk" })
-				map("v", "<leader>hr", function()
+				end, { desc = " Stage Hunk (Visual)" })
+				map("v", "<leader>gr", function()
 					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end, { desc = "Reset Hunk" })
-				map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage Buffer" })
-				map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
-				map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset Buffer" })
-				map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview Hunk" })
-				map("n", "<leader>hb", function()
+				end, { desc = "󰅖 Reset Hunk (Visual)" })
+				map("n", "<leader>gS", gs.stage_buffer, { desc = " Stage Entire Buffer" })
+				map("n", "<leader>gu", gs.undo_stage_hunk, { desc = "󰅖 Undo Last Staged Hunk" })
+				map("n", "<leader>gR", gs.reset_buffer, { desc = "󰅖 Reset Entire Buffer" })
+				map("n", "<leader>gp", gs.preview_hunk, { desc = " Preview Hunk Changes" })
+				map("n", "<leader>gb", function()
 					gs.blame_line({ full = true })
-				end, { desc = "Blame Line" })
-				map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle Line Blame" })
-				map("n", "<leader>hd", gs.diffthis, { desc = "Diff This" })
-				map("n", "<leader>hD", function()
+				end, { desc = "󰬘 Show Git Blame" })
+				map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "󰬘 Toggle Line Blame" })
+				map("n", "<leader>gd", gs.diffthis, { desc = "󰩄 Show Diff (Current)" })
+				map("n", "<leader>gD", function()
 					gs.diffthis("~")
-				end, { desc = "Diff This ~" })
-				map("n", "<leader>td", gs.toggle_deleted, { desc = "Toggle Deleted" })
+				end, { desc = "󰩄 Show Diff (Last Commit)" })
+				map("n", "<leader>td", gs.toggle_deleted, { desc = "󰍶 Toggle Deleted Lines" })
 
 				-- Text object
-				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select Hunk" })
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = " Select Git Hunk" })
 			end,
 		},
 	},
