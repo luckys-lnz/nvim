6b56e94ee04956972ac3a26ec3cc1c0270230e2d
luckys-lnz <lalucky22@gmail.com>
2025-04-02 04:25:39 +0000

    cleaned file 😊

--- init.lua
+++ init.lua
@@ -26,20 +26,3 @@ require("config.autocmds")
 
 -- Setup lazy.nvim
 require("config.lazy")
--- -- Load Catppuccin and apply transparency settings
--- require("plugins.colorscheme")
--- -- vim.cmd.colorscheme("catppuccin")
--- --
--- vim.api.nvim_create_autocmd("ColorScheme", {
--- 	pattern = "catppuccin",
--- 	callback = function()
--- 		vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
--- 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
--- 		vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE", ctermbg = "NONE" })
--- 		vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE", ctermbg = "NONE" })
--- 		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE", ctermbg = "NONE" })
--- 		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE", ctermbg = "NONE" })
--- 		vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE", ctermbg = "NONE" })
--- 		vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE", ctermbg = "NONE" })
--- 	end,
--- })
