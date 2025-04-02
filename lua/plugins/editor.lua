92ab949b6797d1586bc09b3d94da26b06a5eaf85
luckys-lnz <lalucky22@gmail.com>
2025-04-02 23:01:14 +0000

    removed multi-cursor

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -3,18 +3,6 @@
 
 return {
 
-	-- VS Code-like multi-cursor
-	{
-		"mg979/vim-visual-multi",
-		event = "BufReadPost",
-		init = function()
-			vim.g.VM_maps = {
-				["Find Under"] = "<C-d>",
-				["Find Subword Under"] = "<C-d>",
-			}
-		end,
-	},
-
 	-- VS Code-like comment
 	{
 		"numToStr/Comment.nvim",
