621b55de106e19e459dd924ee880ba5f42b54e22
luckys-lnz <lalucky22@gmail.com>
2025-04-02 13:14:27 +0000

    separated configs, moved some config for better readability and SOC

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -3,24 +3,6 @@
 
 return {
 
-	-- VS Code-like lazygit integration
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		keys = {
-			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
-
 	-- VS Code-like multi-cursor
 	{
 		"mg979/vim-visual-multi",
